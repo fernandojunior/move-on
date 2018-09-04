69c39ebb17c1bf8004e23e23b6569bd8a2b0fd89
Fernando Felix <fernandojr.ifcg@live.com>
2018-09-04 17:55:44 -0300

    Configurar redux store

--- src/index.dev.js
+++ src/index.dev.js
@@ -1,20 +1,24 @@
 import 'react-hot-loader/patch'
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 import { AppContainer } from 'react-hot-loader'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
+import createStore from './store'
 import 'bootstrap/dist/css/bootstrap.css'
 import './index.css'
 
-
 const mountPoint = document.getElementById('root')
+const store = createStore(window.__INITIAL_STATE__)
 
 ReactDOM.render(
   <AppContainer>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </AppContainer>,
   mountPoint
 )
---
+++ src/reducer/index.js
@@ -0,0 +1,11 @@
+// import { actionTypes } from './actions'
+
+function reducer(state = null, action) { // eslint-disable-line
+  // switch (action.type) {
+  //   default:
+  //     return state
+  // }
+  return state
+}
+
+export default reducer
